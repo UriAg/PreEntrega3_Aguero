9b84c64d2bfa82985231967a00ba61752e86a1f2
Uriel Agüero <uriel.aguero1812@gmail.com>
2023-05-08 23:42:04 -0300

    loader added

---
+++ app/loader.js
@@ -0,0 +1,6 @@
+const loader = document.querySelector('.loader-container');
+
+document.addEventListener('DOMContentLoaded', function(){
+    loader.style.visibility = 'hidden';
+    loader.style.opacity = '0';
+})
